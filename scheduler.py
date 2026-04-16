5821beee82186f0fa60daca9147781f7b4889fec
Ɗᴇᴇᴘᴀɴꜱʜᴜ <stddeepanshu@gmail.com>
2026-04-16 23:41:42 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -2,7 +2,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
 from config import START_HOUR, END_HOUR, INTERVAL_MIN
 from database import users
-from bot import send_revision_to_user
 
 scheduler = BackgroundScheduler()
 
@@ -10,14 +9,14 @@ def can_send():
     h = datetime.now().hour
     return START_HOUR <= h <= END_HOUR
 
-def job():
-    if not can_send():
-        return
+def start(send_function):
+    def job():
+        if not can_send():
+            return
 
-    for u in users.find():
-        send_revision_to_user(u["user_id"])
+        for u in users.find():
+            send_function(u["user_id"])
 
-def start():
     scheduler.add_job(
         job,
         'interval',
