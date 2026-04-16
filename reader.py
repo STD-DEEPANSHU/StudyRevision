b452ad9915d3ccb6300a54cba6c0c4ba2bd814a7
Ɗᴇᴇᴘᴀɴꜱʜᴜ <stddeepanshu@gmail.com>
2026-04-16 21:18:20 +0530

    Update reader.py

--- reader.py
+++ reader.py
@@ -4,16 +4,26 @@ from bs4 import BeautifulSoup
 
 def read_pdf(file):
     text = ""
-    with pdfplumber.open(file) as pdf:
-        for page in pdf.pages:
-            if page.extract_text():
-                text += page.extract_text()
+    try:
+        with pdfplumber.open(file) as pdf:
+            for page in pdf.pages:
+                t = page.extract_text()
+                if t:
+                    text += t
+    except Exception as e:
+        print("PDF ERROR:", e)
     return text
 
+
 def read_website(url):
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, "html.parser")
-    return soup.get_text()
+    try:
+        res = requests.get(url, timeout=10)
+        soup = BeautifulSoup(res.text, "html.parser")
+        return soup.get_text()
+    except Exception as e:
+        print("WEB ERROR:", e)
+        return ""
+
 
 def read_text(text):
     return text
