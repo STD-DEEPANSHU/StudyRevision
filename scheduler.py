eae5fc47696a8b23a8273f877185f0a79a1b6cc4
Ɗᴇᴇᴘᴀɴꜱʜᴜ <stddeepanshu@gmail.com>
2026-04-16 21:18:39 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,19 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
-from config import START_HOUR, END_HOUR, SEND_INTERVAL_HOURS
+from config import START_HOUR, END_HOUR, SEND_INTERVAL_MINUTES
+
+scheduler = BackgroundScheduler()
 
 def can_send():
     now = datetime.now().hour
     return START_HOUR <= now <= END_HOUR
 
 def start_scheduler(send_function):
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(send_function, 'interval', hours=SEND_INTERVAL_HOURS)
+    scheduler.add_job(
+        send_function,
+        'interval',
+        minutes=SEND_INTERVAL_MINUTES,
+        max_instances=1,
+        coalesce=True
+    )
     scheduler.start()
