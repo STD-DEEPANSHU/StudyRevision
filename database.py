006574e6788203dd2553837548f8e0c204da292b
Ɗᴇᴇᴘᴀɴꜱʜᴜ <stddeepanshu@gmail.com>
2026-04-16 21:17:35 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,9 +3,11 @@ import random
 questions_db = []
 
 def add_questions(q):
-    questions_db.append(q)
+    if q and isinstance(q, str):
+        questions_db.append(q)
 
 def get_question():
     if not questions_db:
-        return "No questions yet"
+        return None
+    
     return random.choice(questions_db)
