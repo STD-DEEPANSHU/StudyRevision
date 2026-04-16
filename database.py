68656ea604748d890a885494bbd6dddc9041828a
Ɗᴇᴇᴘᴀɴꜱʜᴜ <stddeepanshu@gmail.com>
2026-04-16 22:38:14 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from config import MONGO_URI
+from datetime import datetime, timedelta
 import random
 
 client = MongoClient(MONGO_URI)
@@ -7,26 +8,61 @@ db = client["revision_bot"]
 
 users = db["users"]
 questions = db["questions"]
+memory = db["memory"]
 
 def add_user(user_id):
     users.update_one({"user_id": user_id}, {"$set": {"user_id": user_id}}, upsert=True)
 
-def add_question(user_id, q):
-    questions.insert_one({
+# ---------- MEMORY ----------
+def save_memory(user_id, text):
+    memory.update_one(
+        {"user_id": user_id},
+        {"$push": {"messages": text}},
+        upsert=True
+    )
+
+def get_memory(user_id):
+    data = memory.find_one({"user_id": user_id})
+    if not data:
+        return ""
+    return "\n".join(data["messages"][-10:])
+
+# ---------- QUESTIONS ----------
+def add_questions(user_id, qa_list):
+    for qa in qa_list:
+        questions.insert_one({
+            "user_id": user_id,
+            "text": qa,
+            "used_count": 0,
+            "next_review": datetime.now()
+        })
+
+def get_revision(user_id):
+    now = datetime.now()
+    qs = list(questions.find({
         "user_id": user_id,
-        "q": q,
-        "used": False
-    })
+        "next_review": {"$lte": now}
+    }))
 
-def get_questions(user_id):
-    new_q = list(questions.find({"user_id": user_id, "used": False}))
-    old_q = list(questions.find({"user_id": user_id, "used": True}))
+    random.shuffle(qs)
+    return qs[:5]
 
-    result = []
+def update_question(q):
+    level = q["used_count"]
 
-    if new_q:
-        result += random.sample(new_q, min(3, len(new_q)))
-    if old_q:
-        result += random.sample(old_q, min(2, len(old_q)))
+    if level == 0:
+        days = 1
+    elif level == 1:
+        days = 3
+    elif level == 2:
+        days = 7
+    else:
+        days = 15
 
-    return result
+    questions.update_one(
+        {"_id": q["_id"]},
+        {
+            "$inc": {"used_count": 1},
+            "$set": {"next_review": datetime.now() + timedelta(days=days)}
+        }
+    )
