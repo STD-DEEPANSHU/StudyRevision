d04a7aa6ce15424f81ea4b51d8376bc0acd5a849
Ɗᴇᴇᴘᴀɴꜱʜᴜ <stddeepanshu@gmail.com>
2026-04-16 22:23:40 +0530

    Update database.py

--- database.py
+++ database.py
@@ -8,21 +8,25 @@ db = client["revision_bot"]
 users = db["users"]
 questions = db["questions"]
 
-def save_user(user_id):
-    users.update_one(
-        {"user_id": user_id},
-        {"$set": {"user_id": user_id}},
-        upsert=True
-    )
+def add_user(user_id):
+    users.update_one({"user_id": user_id}, {"$set": {"user_id": user_id}}, upsert=True)
 
 def add_question(user_id, q):
     questions.insert_one({
         "user_id": user_id,
-        "q": q
+        "q": q,
+        "used": False
     })
 
-def get_question(user_id):
-    qs = list(questions.find({"user_id": user_id}))
-    if not qs:
-        return None
-    return random.choice(qs)["q"]
+def get_questions(user_id):
+    new_q = list(questions.find({"user_id": user_id, "used": False}))
+    old_q = list(questions.find({"user_id": user_id, "used": True}))
+
+    result = []
+
+    if new_q:
+        result += random.sample(new_q, min(3, len(new_q)))
+    if old_q:
+        result += random.sample(old_q, min(2, len(old_q)))
+
+    return result
