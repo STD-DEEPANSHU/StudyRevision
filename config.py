ed7f303b403bea75f57efeb222928fbaf8327aed
Ɗᴇᴇᴘᴀɴꜱʜᴜ <stddeepanshu@gmail.com>
2026-04-16 21:21:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+MONGO_URI = os.getenv("MONGO_URI")
 
-SEND_INTERVAL_HOURS = 2
 START_HOUR = 10
 END_HOUR = 22
+SEND_INTERVAL_MIN = 120
